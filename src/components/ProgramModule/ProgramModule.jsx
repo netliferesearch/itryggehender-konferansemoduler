a55264e92fdd383a522c11ad83b9b00b6c4cdc43
Andreas Naustdal <andreas.naustdal@netlife.com>
2022-05-24 17:35:10 +0200

    add program module work in progress

---
+++ src/components/ProgramModule/ProgramModule.jsx
@@ -0,0 +1,157 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Richtext from "../Richtext";
+import ArrowAccordion from "../../img/icons/arrow-accordion.svg";
+import Clock from "../../img/icons/clock.svg";
+import Place from "../../img/icons/place.svg";
+
+const ProgramModule = ({
+  overskrift = "",
+  ingress = "",
+  programposter = [],
+}) => {
+  const slugify = (string) =>
+    string
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "");
+
+  return (
+    <div className="container">
+      <div className="row">
+        {/* <div className="col-lg-7 mb-5">
+          <h1 className="mb-5">{overskrift}</h1>
+          <p>{ingress}</p>
+        </div>
+
+        <Richtext title={overskrift} body={ingress} /> */}
+
+        {/* <div className="col-lg-7 mb-5">
+          <h2>Navn på bolk, feks Åpning</h2>
+          <p>
+            Det må være mulig å skrive noen få setninger her om bolken eller noe
+            annet som er relevant for denne bolken av program. Men også mulig
+            uten undertekst, slik som vist under under bolk “parallelsesjoner”
+          </p>
+        </div> */}
+
+        <div className="accordion" id="program-bolk-1">
+          {programposter.map(
+            ({
+              klokkeslett,
+              foredragsholder,
+              foredragTittel,
+              lokale,
+              omForedraget,
+              bilde,
+              temaer,
+              lesMerLink,
+              foredragsholdere,
+            }) => (
+              <section key={foredragTittel} className="c-program c-collapsible">
+                <button
+                  className="c-collapsible__button"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target={`#collapsePost${slugify(foredragTittel)}`}
+                  aria-expanded="false"
+                  aria-controls={`collapsePost${slugify(foredragTittel)}`}
+                >
+                  <span>
+                    <span className="c-program__meta">
+                      <img className="" src={Clock} alt="" />
+                      {klokkeslett}
+                    </span>
+
+                    <span className="c-program__meta">
+                      <img className="" src={Place} alt="" />
+                      {lokale}
+                    </span>
+                  </span>
+
+                  <img
+                    className="c-collapsible__button-arrow"
+                    src={ArrowAccordion}
+                    alt=""
+                  />
+                </button>
+
+                <h3 className="c-program__heading">{foredragTittel}</h3>
+                <div className="c-program__main-lecturer">
+                  {foredragsholder}
+                </div>
+                <ul className="c-program__themes">
+                  {temaer.map(({ tema, color }) => (
+                    <li key={tema}>
+                      <div
+                        className="c-program__themes-color"
+                        style={{ backgroundColor: color }}
+                      ></div>
+                      {tema}
+                    </li>
+                  ))}
+                </ul>
+                <div
+                  className="c-collapsible__body collapse"
+                  id={`collapsePost${slugify(foredragTittel)}`}
+                  data-parent="#program-bolk-1"
+                >
+                  {bilde && <img className="mt-4" src={bilde} alt="" />}
+                  <div className="c-program__description">{omForedraget}</div>
+                  <p>
+                    <a href={lesMerLink}>Les mer om foredragsholderene her</a>
+                  </p>
+                  <ul className="c-program__lecturers">
+                    {foredragsholdere.map(
+                      ({ navn, beskrivelse, bilde }, index) => (
+                        <li key={index} className="c-program__lecturer">
+                          <img
+                            className="c-program__lecturer-image"
+                            src={bilde}
+                            alt=""
+                          />
+                          <div className="c-program__lecturer-name">{navn}</div>
+                          <div className="c-program__lecturer-description">
+                            {beskrivelse}
+                          </div>
+                        </li>
+                      )
+                    )}
+                  </ul>
+                </div>
+              </section>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProgramModule.propTypes = {
+  overskrift: PropTypes.string,
+  ingress: PropTypes.string,
+  programposter: PropTypes.arrayOf(
+    PropTypes.shape({
+      startdato: PropTypes.string,
+      sluttdato: PropTypes.string,
+      foredragsholder: PropTypes.string,
+      foredragTittel: PropTypes.string,
+      lokale: PropTypes.string,
+      omForedraget: PropTypes.string,
+      bilde: PropTypes.string,
+      temaer: PropTypes.arrayOf(PropTypes.object),
+      lesMerLink: PropTypes.string,
+      foredragsholdere: PropTypes.arrayOf(
+        PropTypes.shape({
+          navn: PropTypes.string,
+          stilling: PropTypes.string,
+          selskap: PropTypes.string,
+          bilde: PropTypes.string,
+        })
+      ),
+    })
+  ),
+};
+
+export default ProgramModule;
---
+++ src/components/ProgramModule/ProgramModule.stories.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { default as ProgramModuleComponent } from "./ProgramModule";
+
+// export default {
+//   title: "Components/Program Module (WORK IN PROGRESS)",
+//   component: ProgramModuleComponent,
+// };
+
+export const ProgramModule = ({ ...args }) => (
+  <ProgramModuleComponent {...args} />
+);
+
+const generateProgramPosts = ({ amount }) => {
+  let posts = [];
+
+  for (let index = 0; index < amount; index++) {
+    posts.push({
+      klokkeslett: `${(9 + index).toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+      })}:50`,
+      foredragsholder: "Ola Erik Nordmann, firma",
+      foredragTittel: `Navnet på foredrag ${index} kommer her og kan være over flere linjer, men bør ikke være for langt`,
+      lokale: "Stedet det holdes",
+      omForedraget:
+        "Ingress om foredraget kommer frem når man trykker seg inn på foredraget og kan være lengre enn navnet på foredraget. Da kan man få muligheten til å fortelle mer i dybden og få frem viktige poenger, man kan også skrive en setning om foredragsholderen under.",
+      // bilde: "https://picsum.photos/400/300",
+      temaer: [
+        {
+          tema: "Navn på tema 1",
+          color: "#FE6100",
+        },
+        {
+          tema: "Navn på tema 2",
+          color: "#1E0072",
+        },
+        {
+          tema: "Navn på tema 3",
+          color: "#00625D",
+        },
+        {
+          tema: "Navn på tema 4",
+          color: "#615046",
+        },
+      ],
+      lesMerLink: "https://www.itryggehender24-7.no/",
+      foredragsholdere: [
+        {
+          navn: "Ola Erik Nordmann,",
+          beskrivelse: "Universitetssykehuset Øst Østfold",
+          bilde: "https://picsum.photos/144",
+        },
+        {
+          navn: "Ola Erik Nordmann,",
+          beskrivelse: "Universitetssykehuset Øst Østfold",
+          bilde: "https://picsum.photos/144",
+        },
+        {
+          navn: "Ola Erik Nordmann,",
+          beskrivelse: "Universitetssykehuset Øst Østfold",
+          bilde: "https://picsum.photos/144",
+        },
+        {
+          navn: "Ola Erik Nordmann,",
+          beskrivelse: "Universitetssykehuset Øst Østfold",
+          bilde: "https://picsum.photos/144",
+        },
+        {
+          navn: "Ola Erik Nordmann,",
+          beskrivelse: "Universitetssykehuset Øst Østfold",
+          bilde: "https://picsum.photos/144",
+        },
+      ],
+    });
+  }
+
+  return posts;
+};
+
+ProgramModule.args = {
+  overskrift: "Dag 1: Heading kommer her",
+  ingress:
+    "Her er det plass til en ingress om man trenger det. Her er det plass til en ingress om man trenger det. Her er det plass til en ingress om man trenger det. Her er det plass til en ingress om man trenger det.",
+  programposter: generateProgramPosts({ amount: 3 }),
+};
