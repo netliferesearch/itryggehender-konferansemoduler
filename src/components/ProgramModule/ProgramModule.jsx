61ea0ac20f0346f1defe97fb311f6bf49974d30c
Andreas Naustdal <andreas.naustdal@netlife.com>
2022-05-25 10:44:21 +0200

    add section and style spacing

--- src/components/ProgramModule/ProgramModule.jsx
+++ src/components/ProgramModule/ProgramModule.jsx
@@ -13,123 +13,124 @@ const ProgramModule = ({ overskrift = "", ingress = "", bolker = [] }) => {
       .replace(/[^\w-]+/g, "");
 
   return (
-    <div className="container">
-      <div className="row">
-        <Richtext title={overskrift} body={ingress} offset />
-
-        {bolker.map(({ bolkNavn, bolkIngress, programposter }) => (
-          <>
-            <div className="col-lg-7 mb-5">
-              <h2>{bolkNavn}</h2>
-              <p>{bolkIngress}</p>
-            </div>
-            <div className="accordion" id={slugify(bolkNavn)}>
-              {programposter.map(
-                ({
-                  klokkeslett,
-                  foredragsholder,
-                  foredragTittel,
-                  lokale,
-                  omForedraget,
-                  bilde,
-                  temaer,
-                  lesMerLink,
-                  foredragsholdere,
-                }) => (
-                  <section
-                    key={foredragTittel}
-                    className="c-program c-collapsible"
-                  >
-                    <button
-                      className="c-collapsible__button collapsed"
-                      type="button"
-                      data-toggle="collapse"
-                      data-target={`#collapsePost${slugify(foredragTittel)}`}
-                      aria-expanded="false"
-                      aria-controls={`collapsePost${slugify(foredragTittel)}`}
+    <>
+      <Richtext title={overskrift} body={ingress} offset />
+      <div className="container">
+        <div className="row">
+          {bolker.map(({ bolkNavn, bolkIngress, programposter }) => (
+            <section className="mb-5">
+              <div className="col-lg-7 mb-5">
+                <h2 className="c-program__heading">{bolkNavn}</h2>
+                <p>{bolkIngress}</p>
+              </div>
+              <div className="accordion" id={slugify(bolkNavn)}>
+                {programposter.map(
+                  ({
+                    klokkeslett,
+                    foredragsholder,
+                    foredragTittel,
+                    lokale,
+                    omForedraget,
+                    bilde,
+                    temaer,
+                    lesMerLink,
+                    foredragsholdere,
+                  }) => (
+                    <section
+                      key={foredragTittel}
+                      className="c-program c-collapsible"
                     >
-                      <span>
-                        {klokkeslett && (
-                          <span className="c-program__meta">
-                            <img className="" src={Clock} alt="" />
-                            {klokkeslett}
-                          </span>
-                        )}
-
-                        {lokale && (
-                          <span className="c-program__meta">
-                            <img className="" src={Place} alt="" />
-                            {lokale}
-                          </span>
-                        )}
-                      </span>
-
-                      <img
-                        className="c-collapsible__button-arrow"
-                        src={ArrowAccordion}
-                        alt=""
-                      />
-                    </button>
-
-                    <div className="c-collapsible__body">
-                      <h3 className="c-program__heading">{foredragTittel}</h3>
-                      <div className="c-program__main-lecturer">
-                        {foredragsholder}
-                      </div>
-                      <ul className="c-program__themes">
-                        {temaer.map(({ tema, color }) => (
-                          <li key={tema}>
-                            <div
-                              className="c-program__themes-color"
-                              style={{ backgroundColor: color }}
-                            ></div>
-                            {tema}
-                          </li>
-                        ))}
-                      </ul>
-                      <div
-                        className="collapse"
-                        id={`collapsePost${slugify(foredragTittel)}`}
-                        data-parent={`#${slugify(bolkNavn)}`}
+                      <button
+                        className="c-collapsible__button collapsed"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target={`#collapsePost${slugify(foredragTittel)}`}
+                        aria-expanded="false"
+                        aria-controls={`collapsePost${slugify(foredragTittel)}`}
                       >
-                        {bilde && <img className="mt-4" src={bilde} alt="" />}
-                        <div className="c-program__description">
-                          {omForedraget}
-                        </div>
-                        <p>
-                          <a href={lesMerLink}>
-                            Les mer om foredragsholderene her
-                          </a>
-                        </p>
-                        <ul className="c-program__lecturers">
-                          {foredragsholdere.map(
-                            ({ navn, beskrivelse, bilde }, index) => (
-                              <li key={index} className="c-program__lecturer">
-                                <img
-                                  className="c-program__lecturer-image"
-                                  src={bilde}
-                                  alt=""
-                                />
-                                <div className="c-program__lecturer-name">
-                                  {navn}
-                                </div>
-                                <div className="c-program__lecturer-description">
-                                  {beskrivelse}
-                                </div>
-                              </li>
-                            )
+                        <span>
+                          {klokkeslett && (
+                            <span className="c-program__meta">
+                              <img className="" src={Clock} alt="" />
+                              {klokkeslett}
+                            </span>
                           )}
+
+                          {lokale && (
+                            <span className="c-program__meta">
+                              <img className="" src={Place} alt="" />
+                              {lokale}
+                            </span>
+                          )}
+                        </span>
+
+                        <img
+                          className="c-collapsible__button-arrow"
+                          src={ArrowAccordion}
+                          alt=""
+                        />
+                      </button>
+
+                      <div className="c-collapsible__body">
+                        <h3 className="c-program__heading">{foredragTittel}</h3>
+                        <div className="c-program__main-lecturer">
+                          {foredragsholder}
+                        </div>
+                        <ul className="c-program__themes">
+                          {temaer.map(({ tema, color }) => (
+                            <li key={tema}>
+                              <div
+                                className="c-program__themes-color"
+                                style={{ backgroundColor: color }}
+                              ></div>
+                              {tema}
+                            </li>
+                          ))}
                         </ul>
+                        <div
+                          className="collapse"
+                          id={`collapsePost${slugify(foredragTittel)}`}
+                          data-parent={`#${slugify(bolkNavn)}`}
+                        >
+                          {bilde && <img className="mt-4" src={bilde} alt="" />}
+                          <div className="c-program__description">
+                            {omForedraget}
+                          </div>
+                          <p>
+                            <a href={lesMerLink}>
+                              Les mer om foredragsholderene her
+                            </a>
+                          </p>
+                          <ul className="c-program__lecturers">
+                            {foredragsholdere.map(
+                              ({ navn, beskrivelse, bilde }, index) => (
+                                <li key={index} className="c-program__lecturer">
+                                  <img
+                                    className="c-program__lecturer-image"
+                                    src={bilde}
+                                    alt=""
+                                  />
+                                  <div className="c-program__lecturer-name">
+                                    {navn}
+                                  </div>
+                                  <div className="c-program__lecturer-description">
+                                    {beskrivelse}
+                                  </div>
+                                </li>
+                              )
+                            )}
+                          </ul>
+                        </div>
                       </div>
-                    </div>
-                  </section>
-                )
-              )}
-            </div>
-          </>
-        ))}
+                    </section>
+                  )
+                )}
+              </div>
+            </section>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
