e69e53b7df402cb767b62f1d017aa782a574ec97
Andreas Naustdal <andreas.naustdal@netlife.com>
2022-05-25 10:03:33 +0200

    hide clock and place in some posts

--- src/components/ProgramModule/ProgramModule.stories.jsx
+++ src/components/ProgramModule/ProgramModule.stories.jsx
@@ -15,13 +15,15 @@ const generateProgramPosts = ({ amount }) => {
 
   for (let index = 0; index < amount; index++) {
     posts.push({
-      klokkeslett: `${(9 + index).toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false,
-      })}:50`,
+      ...(index !== 2 && {
+        klokkeslett: `${(9 + index).toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}:50`,
+      }),
       foredragsholder: "Ola Erik Nordmann, firma",
       foredragTittel: `Navnet på foredrag ${index} kommer her og kan være over flere linjer, men bør ikke være for langt`,
-      lokale: "Stedet det holdes",
+      ...((index + 1) % 2 && { lokale: "Stedet det holdes" }),
       omForedraget:
         "Ingress om foredraget kommer frem når man trykker seg inn på foredraget og kan være lengre enn navnet på foredraget. Da kan man få muligheten til å fortelle mer i dybden og få frem viktige poenger, man kan også skrive en setning om foredragsholderen under.",
       // bilde: "https://picsum.photos/400/300",
