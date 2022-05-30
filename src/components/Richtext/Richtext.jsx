6a3a27703e373b844a5e2bb91fc5af935e22c5f2
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2022-05-30 14:43:35 +0200

    fix bad path

--- src/components/Richtext/Richtext.jsx
+++ src/components/Richtext/Richtext.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading } from "src/elements/Heading.jsx";
+import { Heading } from "../../elements/Heading.jsx";
 
 export const Richtext = ({
   offset = false,
