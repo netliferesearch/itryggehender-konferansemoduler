71d891def7c0122ca6150d08bea092da7e9c6184
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2022-05-30 16:10:25 +0200

    add clas prop to heading component

--- src/elements/Heading.jsx
+++ src/elements/Heading.jsx
@@ -1,17 +1,17 @@
-export const Heading = ({ level = "h2", children }) => {
+export const Heading = ({ classes="", level = "h2", children }) => {
   switch (level) {
     case "h1":
-      return <h1>{children}</h1>;
+      return <h1 className={classes}>{children}</h1>;
     case "h2":
-      return <h2>{children}</h2>;
+      return <h2 className={classes}>{children}</h2>;
     case "h3":
-      return <h3>{children}</h3>;
+      return <h3 className={classes}>{children}</h3>;
     case "h4":
-      return <h4>{children}</h4>;
+      return <h4 className={classes}>{children}</h4>;
     case "h5":
-      return <h5>{children}</h5>;
+      return <h5 className={classes}>{children}</h5>;
     case "h6":
-      return <h6>{children}</h6>;
+      return <h6 className={classes}>{children}</h6>;
     default:
       return null;
   }
