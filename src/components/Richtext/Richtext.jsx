c381b04032fa6e40446fdaa38bb5a01b6648dcd0
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2022-05-30 14:38:26 +0200

    move Heading component into its own file

--- src/components/Richtext/Richtext.jsx
+++ src/components/Richtext/Richtext.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Heading } from "src/elements/Heading.jsx";
 
 export const Richtext = ({
   offset = false,
@@ -7,25 +8,6 @@ export const Richtext = ({
   body = "",
   children,
 }) => {
-  
-  const Heading = ({ level = "h2", children }) => {
-    switch (level) {
-      case "h1":
-        return <h1>{children}</h1>;
-      case "h2":
-        return <h2>{children}</h2>;
-      case "h3":
-        return <h3>{children}</h3>;
-      case "h4":
-        return <h4>{children}</h4>;
-      case "h5":
-        return <h5>{children}</h5>;
-      case "h6":
-        return <h6>{children}</h6>;
-      default:
-        return null;
-    }
-  };
 
   const centerStyle = "offset-lg-3 offset-md-1 offset-sm-0";
   return (
