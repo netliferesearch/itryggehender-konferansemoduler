10b54dae6d6596fe5aabef6526e5e57017e284a7
Andreas Naustdal <andreas.naustdal@netlife.com>
2022-05-31 14:10:52 +0200

    add story without content

--- src/components/ProgramModule/ProgramModule.stories.jsx
+++ src/components/ProgramModule/ProgramModule.stories.jsx
@@ -7,6 +7,46 @@ export default {
   component: ProgramModuleComponent,
 };
 
+export const WithoutContent = ({ ...args }) => (
+  <ProgramModuleComponent {...args} />
+);
+
+WithoutContent.args = {
+  overskrift: "overskrift",
+  ingress: "ingress",
+  bolker: [
+    {
+      bolkNavn: "blockName",
+      bolkIngress: "blockIngress",
+      programposter: [
+        {
+          klokkeslett: "klokkeslett",
+          foredragsholder: "foredragsholder",
+          foredragTittel: `foredragTittel`,
+          lokale: "lokale",
+          omForedraget: "omForedraget",
+          // bilde: "https://picsum.photos/400/300",
+          temaer: [
+            {
+              tema: "tema",
+              colorClass: "orange",
+            },
+          ],
+          lesMerLink: "https://www.itryggehender24-7.no/",
+          foredragsholdere: [
+            {
+              navn: "navn",
+              stilling: "stilling",
+              selskap: "selskap",
+              bilde: "https://picsum.photos/144",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
 export const ProgramModule = ({ ...args }) => (
   <ProgramModuleComponent {...args} />
 );
